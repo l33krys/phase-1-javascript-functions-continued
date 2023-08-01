01b03947f6eb186041993c3e78dc9805411cadf5
Krystle Lee <l33.krys@gmail.com>
2023-07-31 17:36:16 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = `go to the office`) {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = `*`) {
+    return function (message = `special`) {
+        return `You are ${flair}${message}${flair}!`;
+    };
+}
+
+wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+
